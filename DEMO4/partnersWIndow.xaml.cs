f5ab9422a6643a94bfefb30bc3ca747810bb98a7
Zhenya <74973491+RedEnder666@users.noreply.github.com>
2025-06-09 00:37:59 +0300

    edit1

--- DEMO4/partnersWIndow.xaml.cs
+++ DEMO4/partnersWIndow.xaml.cs
@@ -1,45 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.Migrations;
+using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using DEMO4.Utils;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace DEMO4
 {
-    /// <summary>
-    /// Interaction logic for partnersWindow.xaml
-    /// </summary>
-    
-
-
     public partial class partnersWindow : Window
     {
-        private bool isEditing = false; 
-        private partner currentlyEditingPartner = null; 
-
-        public void updateState(StackPanel sp, partner p)
+        public partnersWindow()
         {
-            isEditing = !isEditing;
-            sp.Children[1].IsEnabled = isEditing;
-            sp.Children[3].IsEnabled = isEditing;
-            sp.Children[5].IsEnabled = isEditing;
-            sp.Children[7].IsEnabled = isEditing;
-            var editBtn = (Button)sp.Children[8];
-            editBtn.Content = isEditing ? "Отменить" : "Редактировать";
-            var delBtn = (Button)sp.Children[9];
-            delBtn.Content = isEditing ? "Сохранить" : "Удалить";
+            InitializeComponent();
+            LoadPartners();
         }
+
         private void LoadPartners()
         {
             PartnerList.Children.Clear();
@@ -48,106 +27,223 @@ namespace DEMO4
             {
                 foreach (var p in db.partners.ToList())
                 {
-                    var total = CostCalc.Calculate(db, p);
-
-                    var b = new Border
-                    {
-                        BorderBrush = Brushes.Black,
-                        BorderThickness = new Thickness(2),
-                        Margin = new Thickness(15)
-                    };
-
-                    var sp = new StackPanel();
-                    var nameBox = new TextBox
-                    {
-                        Text = p.name,
-                        Margin = new Thickness(5),
-                        IsEnabled = false,
-                    };
-                    var addressBox = new TextBox { Text = p.address, Margin = new Thickness(5), IsEnabled = false };
-                    var phoneBox = new TextBox { Text = p.phone, Margin = new Thickness(5), IsEnabled = false };
-                    var typeBox = new TextBox
-                    {
-                        Text = p.type.ToString(),
-                        Margin = new Thickness(5),
-                        IsEnabled = false,
-                        
-                    };
-                    sp.Children.Add(new TextBlock { Text = "Имя:", Margin = new Thickness(5) });
-                    sp.Children.Add(nameBox);
-                    sp.Children.Add(new TextBlock { Text = "Адрес:", Margin = new Thickness(5) });
-                    sp.Children.Add(addressBox);
-                    sp.Children.Add(new TextBlock { Text = "Номер:", Margin = new Thickness(5) });
-                    sp.Children.Add(phoneBox);
-                    sp.Children.Add(new TextBlock { Text = "Тип:", Margin = new Thickness(5) });
-                    sp.Children.Add(typeBox);
-                    
-
-                    sp.Children.Add(new TextBlock { Text = total.ToString() });
-
-                    var deleteBtn = new Button
-                    {
-                        Content = "Удалить",
-                        Margin = new Thickness(5),
-                        Tag = p
-                    };
-                    deleteBtn.Click += (s, e) =>
-                    {
-                        if (isEditing && currentlyEditingPartner == deleteBtn.Tag)
-                        {
-                           using (var db2 = new DEMO_v3Entities())
-                            {
-                                partner partnertd = db2.partners.Find(((partner)deleteBtn.Tag).partner_ID);
-                                partnertd.name = nameBox.Text;
-                                partnertd.address = addressBox.Text;
-                                partnertd.phone = phoneBox.Text;
-                                partnertd.type = byte.Parse(typeBox.Text);
-                                db.SaveChanges();
-                                updateState(sp);
-                            }
-                        }
-                        else
-                        {
-                            var btn = (Button)s;
-                            var partnertd = (partner)btn.Tag;
-                            using (var db2 = new DEMO_v3Entities())
-                            {
-                                db2.partners.Remove(db2.partners.Find(partnertd.partner_ID));
-                                db2.SaveChanges();
-                                LoadPartners();
-                            }
-                        }
-                        
-                    };
-
-                    sp.Children.Add(deleteBtn);
-
-                    var editBtn = new Button
-                    {
-                        Content = "Редактировать",
-                        Margin = new Thickness(5),
-                        Tag = p
-                    };
-                    editBtn.Click += (s, e) =>
-                    {
-                        updateState(sp);
-
-                    };
-                    sp.Children.Add(editBtn);
-
-                    b.Child = sp;
-                    PartnerList.Children.Add(b);
+                    PartnerList.Children.Add(new PartnerCardControl(p, db, RefreshList));
                 }
             }
         }
 
+        private void RefreshList() => LoadPartners();
 
-        public partnersWindow()
+        private void AddPartner_Click(object sender, RoutedEventArgs e)
         {
+            var newPartner = new partner { name = "Новый партнер", type = 1 };
+            using (var db = new DEMO_v3Entities())
+            {
+                db.partners.Add(newPartner);
+                db.SaveChanges();
+                LoadPartners();
+            }
+        }
+    }
 
-            InitializeComponent();
-            LoadPartners();
+    public class PartnerCardControl : Border, INotifyPropertyChanged
+    {
+        private readonly DEMO_v3Entities _db;
+        private readonly Action _refreshCallback;
+        private partner _partnerEntity;
+        private bool _isEditing;
+        private Button _editButton;
+        private Button _actionButton;
 
+        private string _partnerName;
+        private string _address;
+        private string _phone;
+        private string _partnerType;
+
+        public PartnerCardControl(partner partner, DEMO_v3Entities db, Action refreshCallback)
+        {
+            _partnerEntity = partner;
+            _db = db;
+            _refreshCallback = refreshCallback;
+
+            PartnerName = partner.name;
+            Address = partner.address;
+            Phone = partner.phone;
+            PartnerType = partner.type.ToString();
+
+            // UI Setup
+            BorderBrush = Brushes.DarkSlateBlue;
+            BorderThickness = new Thickness(2);
+            CornerRadius = new CornerRadius(8);
+            Margin = new Thickness(10);
+            Background = new SolidColorBrush(Color.FromArgb(15, 70, 130, 180));
+            Padding = new Thickness(10);
+
+            var sp = new StackPanel();
+
+            AddField(sp, "Имя:", nameof(PartnerName));
+            AddField(sp, "Адрес:", nameof(Address));
+            AddField(sp, "Номер:", nameof(Phone));
+            AddField(sp, "Тип:", nameof(PartnerType), true);
+
+            sp.Children.Add(new TextBlock
+            {
+                Text = $"Общий объем продаж: {CostCalc.Calculate(db, partner):C}",
+                FontWeight = FontWeights.Bold,
+                Margin = new Thickness(0, 10, 0, 0)
+            });
+
+            var buttonPanel = new StackPanel { Orientation = Orientation.Horizontal };
+
+            _editButton = new Button
+            {
+                Content = "Редактировать",
+                Margin = new Thickness(0, 10, 5, 0)
+            };
+            _editButton.Click += ToggleEditMode;
+
+            _actionButton = new Button
+            {
+                Content = "Удалить",
+                Margin = new Thickness(5, 10, 0, 0)
+            };
+            _actionButton.Click += ActionButton_Click;
+
+            buttonPanel.Children.Add(_editButton);
+            buttonPanel.Children.Add(_actionButton);
+            sp.Children.Add(buttonPanel);
+
+            Child = sp;
+        }
+
+        public string PartnerName
+        {
+            get => _partnerName;
+            set { _partnerName = value; OnPropertyChanged(); }
+        }
+
+        public string Address
+        {
+            get => _address;
+            set { _address = value; OnPropertyChanged(); }
+        }
+
+        public string Phone
+        {
+            get => _phone;
+            set { _phone = value; OnPropertyChanged(); }
+        }
+
+        public string PartnerType
+        {
+            get => _partnerType;
+            set
+            {
+                if (byte.TryParse(value, out _))
+                {
+                    _partnerType = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private void AddField(StackPanel parent, string label, string propertyName, bool isNumeric = false)
+        {
+            parent.Children.Add(new TextBlock
+            {
+                Text = label,
+                FontWeight = FontWeights.SemiBold,
+                Margin = new Thickness(0, 5, 0, 0)
+            });
+
+            var binding = new Binding(propertyName)
+            {
+                Source = this,
+                Mode = BindingMode.TwoWay,
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+            };
+
+            var textBox = new TextBox
+            {
+                Margin = new Thickness(0, 2, 0, 5),
+                IsEnabled = false
+            };
+
+            if (isNumeric)
+            {
+                textBox.PreviewTextInput += (s, e) =>
+                    e.Handled = !char.IsDigit(e.Text, 0);
+            }
+
+            textBox.SetBinding(TextBox.TextProperty, binding);
+            parent.Children.Add(textBox);
+        }
+
+        private void ToggleEditMode(object sender, RoutedEventArgs e)
+        {
+            _isEditing = !_isEditing;
+
+            foreach (var child in ((StackPanel)Child).Children)
+            {
+                if (child is TextBox tb)
+                    tb.IsEnabled = _isEditing;
+            }
+
+            _editButton.Content = _isEditing ? "Отменить" : "Редактировать";
+            _actionButton.Content = _isEditing ? "Сохранить" : "Удалить";
+        }
+
+        private void ActionButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isEditing)
+            {
+                // SAVE LOGIC
+                try
+                {
+                    using (var db = new DEMO_v3Entities())
+                    {
+                        var partnerToUpdate = db.partners.Find(_partnerEntity.partner_ID);
+                        partnerToUpdate.name = PartnerName;
+                        partnerToUpdate.address = Address;
+                        partnerToUpdate.phone = Phone;
+                        partnerToUpdate.type = byte.Parse(PartnerType);
+                        db.SaveChanges();
+                        _partnerEntity = partnerToUpdate;
+                    }
+                    // PROPERLY exit edit mode
+                    _isEditing = false;
+                    foreach (var child in ((StackPanel)Child).Children)
+                    {
+                        if (child is TextBox tb) tb.IsEnabled = false;
+                    }
+                    _editButton.Content = "Редактировать";
+                    _actionButton.Content = "Удалить";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка сохранения: {ex.Message}");
+                }
+            }
+            else
+            {
+                // DELETE LOGIC
+                if (MessageBox.Show("Удалить этого партнера?", "Подтверждение",
+                    MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    using (var db = new DEMO_v3Entities())
+                    {
+                        db.partners.Remove(db.partners.Find(_partnerEntity.partner_ID));
+                        db.SaveChanges();
+                        _refreshCallback?.Invoke();
+                    }
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
